dd60be822dc7a7e2f739a50c4c7df1f493a08edb
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-06-03 18:21:54 +0300

    feat: setup google fonts

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
+import { AlegreyaSansSC_400Regular } from "@expo-google-fonts/alegreya-sans-sc";
+import { useFonts } from "expo-font";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.sceen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infra/theme";
 
 export default function App() {
+  const [alegreyaLoaded] = useFonts({
+    AlegreyaSansSC_400Regular,
+  });
+
+  const [latoLoaded] = useFonts({
+    Lato_400Regular,
+  });
+
+  if (!latoLoaded || !alegreyaLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
